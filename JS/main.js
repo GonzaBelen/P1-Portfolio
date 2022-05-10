912aa4a3540f3fb6d2ef86437c900c2f871aab41
Gonzalo Belen <gonzalobelen123@gmail.com>
2022-05-10 10:44:01 -0300

    entrega de ejercicios JS

--- JS/main.js
+++ JS/main.js
@@ -17,12 +17,11 @@ function ejercicio1(){
 // ´hola ${usuario}´
 
 function ejercicio2(){
-  console.log((document.getElementById("ejercicio2Num1")))
   const num1 = parseInt(document.getElementById("ejercicio2Num1").value);
   const num2 = parseInt(document.getElementById("ejercicio2Num2").value);
-  const ejercicio2Resultado = document.getElementById("ejecicio2Resultado");
+  const ejercicio2Resultado = document.getElementById("ejercicio2Resultado");
 
-  //ejercicio2Resultado.value = num1 + num2;
+  ejercicio2Resultado.value = num1 + num2;
   
 };
 
@@ -42,12 +41,96 @@ function ejercicio4(){
   const resultado = document.getElementById("ejercicio4Resultado");
   let res = 0;
 
-  if (gradosC > 0) {
+  if (gradosC != 0) {
     res = gradosC * (9 / 5) + 32;
-    resultado.textContent = `Los grados celsius (${gradosC}°C) a fahrenheit son: ${res}`;
-  } else{
+    resultado.textContent = "los grados celcius " + gradosC + " °C a fahrengeit son " + res + " °F";
+  }else{
     res = (gradosF - 32) / 1.8;
-    resultado.textContent = `Los grados fahrenheit (${gradosF}°F) a celsius son: ${res}`;
+    resultado.textContent = "los grados fahrenheit " + gradosF + " °F a celcius son " + res + " °C";
   }
+}
+
+function ejercicio5(){
+  const palabra = document.getElementById("ejercicio5Palabra").value;
+  const resultado = document.getElementById("ejercicio5Resultado");
+  resultado.textContent = "La palabra " + palabra + " tiene " + palabra.length + " letras"
+}
+
+function ejercicio6(){
+  const num = parseInt(document.getElementById("ejercicio6Num").value);
+  const resultado = document.getElementById("ejercicio6Resultado");
+  const resto = (num % 2);
+  if (resto == 0){
+    resultado.textContent = "El numero ingresado es par";
+  }else{
+    resultado.textContent = "El numero ingresado es impar";
+  }
+}
+
+function ejercicio7(){
+  const num1 = parseInt(document.getElementById("ejercicio7Num1").value);
+  const num2 = parseInt(document.getElementById("ejercicio7Num2").value);
+  const resultado = document.getElementById("ejercicio7Resultado");
+
+ 
+  resultado.textContent = "";
   
+  if(num1 < num2){
+    for(let i=num1+1; i < num2; i++){
+      let resto1 = (i % 3);
+      if (resto1 == 0){
+        resultado.textContent = resultado.innerHTML + " | " + i;
+      }
+    }
+  }else{
+    for(let i=num1+1; i < num1; i++){
+      let resto2 = (i % 3);
+      if(resto2 == 0){
+        resultado.textContent = resultado.innerHTML + " | " + i;
+      }
+    }
+  }
+}
+
+function ejercicio8(){
+  const num1 = parseInt(document.getElementById("ejercicio8Num").value);
+  const resultado = document.getElementById("ejercicio8Resultado");
+
+  const esPrimo = (num) => {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+      return true;
+    }
+  }
+  for (let x = 0; x <= num1; x++) {
+    esPrimo(x) && (resultado.textContent = resultado.innerHTML + " | " + x);
+  }
+}
+
+function ejercicio9(){
+  const nombre = document.getElementById("ejercicio9Nombre").value;
+  const apellido = document.getElementById("ejercicio9Apellido").value;
+  const edad = parseInt(document.getElementById("ejercicio9Edad").value);
+  const ciudad = document.getElementById("ejercicio9Ciudad").value;
+  const resultado = document.getElementById("ejercicio9Resultado");
+
+  resultado.textContent = "Mi nombre es " + nombre + " " + apellido + " , tengo " + edad + " años. Nací en la ciudad de " + ciudad;
+}
+
+function ejercicio10(){
+  const num1 = parseInt(document.getElementById("ejercicio10Num1").value);
+  const num2 = parseInt(document.getElementById("ejercicio10Num2").value);
+  const resultado = document.getElementById("ejercicio10Resultado");
+
+  if(num1 < num2){
+    for(let i=num1+1; i < num2; i++){
+        resultado.textContent = resultado.innerHTML + " | " + i;
+      }
+  }else{
+    for(let i=num1+1; i < num1; i++){
+      resultado.textContent = resultado.innerHTML + " | " + i;
+    }
+  }
 }
